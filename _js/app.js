5489486ae5f83cf413c8aac205dae8851e704e19
mihnor <go@wesense.com.br>
2014-05-31 17:57:31 -0300

    correções json e delegando bower_compenets

--- _js/app.js
+++ _js/app.js
@@ -10,17 +10,5 @@ app.controller('TitleController',function(){
   this.noob = gem;
 });
 
-app.controller('MainController',[ '$http',function($http){
-
-  var namesAna = this;
-  namesAna.names = [];
-
-
-
-  $http.get('_json/anaNames.json').success(function(data){
-    namesAna.names = data;
-  });
-
-});
 
 })();
